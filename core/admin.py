c5f7f4ce89a919deab6b7cb0275d6e2d460143a2
Thiago Draeger <thiagodraeger123@gmail.com>
2022-08-29 15:05:20 -0300

    models

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from core.models import Marca, Categoria
+from core.models import Marca, Categoria, Carro
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
+admin.site.register(Carro)
